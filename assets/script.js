f6a8d0007b0da4287e31fc2f374826579743e669
FrancoCarra <104314979+FrancoCarra@users.noreply.github.com>
2022-08-08 17:20:18 +0200

    FORMULARIO

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+const { isValidElement } = require("react")
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
@@ -54,3 +56,22 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*const $form = document.querySelector("#form")
+
+$form.addEventListener('submit',handleSubmit,)
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Su mensaje se envio correctamente! Pronto recibira una respuesta! ;)')
+    }
+}*/
